ed60c491eb8687c4d3cec78fa587e0db8abec9e0
agungd3v <agungd3v@gmail.com>
2020-11-28 23:31:13 +0700

    scrolling pages

--- resources/js/Pages/Content.vue
+++ resources/js/Pages/Content.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="relative h-screen overflow-hidden">
-        <div class="absolute nav-container">
+        <div class="fixed nav-container">
             <ul class="navs flex flex-col justify-center items-center"></ul>
         </div>
         <section class="section section-1 w-full h-screen flex justify-center items-center">
-            <h5 class="text-3xl">This is section 1</h5>
+            <h5 class="text-3xl">This is wrapper 1</h5>
         </section>
         <section class="section section-2 w-full h-screen flex justify-center items-center">
-            <h5 class="text-3xl">This is section 2</h5>
+            <h5 class="text-3xl">This is wrapper 2</h5>
         </section>
         <section class="section section-3 w-full h-screen flex justify-center items-center">
-            <h5 class="text-3xl">This is section 3</h5>
+            <h5 class="text-3xl">This is wrapper 3</h5>
         </section>
         <section class="section section-4 w-full h-screen flex justify-center items-center">
-            <h5 class="text-3xl">This is section 4</h5>
+            <h5 class="text-3xl">This is wrapper 4</h5>
         </section>
     </div>
 </template>
@@ -27,13 +27,52 @@ export default {
     },
     methods: {
         scrollViews() {
+            let eLto = 0
             const sections = document.querySelectorAll('.section')
-            sections.forEach(e => {
+            sections.forEach((e, i) => {
                 const navItem = `<li class="nav-item rounded-full"></li>`
                 document.querySelector('.navs').innerHTML += navItem
             })
-            const itemsNavigator = document.querySelectorAll('.nav-item')
-            itemsNavigator[0].classList.add('active')
+            const navItem = document.querySelectorAll('.nav-item')
+            navItem[eLto].classList.add('active')
+            navItem.forEach((e, i) => {
+                e.addEventListener('click', function() {
+                    navItem[eLto].classList.remove('active')
+                    sections[i].scrollIntoView({
+                        behavior: 'smooth'
+                    })
+                    eLto = i
+                    navItem[eLto].classList.add('active')
+                })
+            })
+            document.querySelector('html, body').addEventListener('wheel', wheel => {
+                wheel.preventDefault()
+                let wheelY = Math.floor(wheel.deltaY)
+                if (wheelY > 0) {
+                    if (eLto === sections.length - 1) {
+                        return false
+                    } else {
+                        navItem[eLto].classList.remove('active')
+                        eLto++
+                        sections[eLto].scrollIntoView({
+                            behavior: 'smooth'
+
+                        })
+                        navItem[eLto].classList.add('active')
+                    }
+                } else {
+                    if (eLto === 0) {
+                        return false
+                    } else {
+                        navItem[eLto].classList.remove('active')
+                        eLto--
+                        sections[eLto].scrollIntoView({
+                            behavior: 'smooth'
+                        })
+                        navItem[eLto].classList.add('active')
+                    }
+                }
+            }, { passive: false })
         },
     },
 }
